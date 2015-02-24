45661c94e1b54efb22b9b388dd3e560407878f6b
Laurel 'Kona' Hart <laurel.elise.hart@gmail.com>
2015-02-23 23:56:51 -0800

    Added pride

--- README.md
+++ README.md
@@ -3,12 +3,18 @@ Make rainbows --- in IRC!
 
 ## Scripts
 ### Spectrum
+Output colored text in a colorscheme, starting at a random point so
+that it will be different with every usage. 
+If the first word of the text is /topic, /kick, or /me, completes those 
+commands with colored text.
 Currently features three colorschemes: rainbow, bright, and cool. 
-Able to handle /msg, /me, /topic, and /kick commands in rainbow color.
-Usage:
+
+#### Usage:
 * /rainbow <text>
 * /bright <text>
 * /cool <text>
+
+
 See image below for demonstration. 
 ![spectrum demonstration](https://cloud.githubusercontent.com/assets/945693/6343589/175d9c0c-bba0-11e4-963a-935dff8bd767.png)
 Note: Colors may appear differently in different terminals.
@@ -16,13 +22,14 @@ Note: Colors may appear differently in different terminals.
 ### Hue
 Output text in command hue. If the first word of the text is /topic, /kick, or 
 /me, completes those commands with colored text.
-Commands:
+
+#### Usage:
 * /white <text>
 * /red <text>
 * /lightred <text>
 	* lightred is also aliased to /boldred <text>
 * /orange <text>
-	*orange is also aliased to /brown <text>
+	* orange is also aliased to /brown <text>
 * /yellow <text>
 * /green <text>
 * /lightgreen <text>
@@ -41,11 +48,35 @@ Commands:
 	* gray is also aliased to /grey <text>
 * /lightgray <text>
 	* lightgray is also aliased to /lightgrey <text>
+
+
 See image below for demonstration. 
 ![hue demonstration](https://cloud.githubusercontent.com/assets/945693/6343939/032cc018-bba6-11e4-8795-65cfac6d9eb1.png)
 Note: Colors may appear differently in different terminals.
 Some colors have multiple aliases in order to accomodate variances in display 
 color and spelling.
+
+### Pride
+Output colored text in a pride/flag colorscheme. Unlike spectrum, these scripts
+always start on the same color.
+If the first word of the text is /topic, /kick, or /me, completes those 
+commands with colored text.
+
+#### Usage:
+* pride <text>
+* /trans <text>
+* /genderqueer <text>
+* /bi <text>
+* /pan <text>
+* /poly <text>
+* /ace <text>
+* /lightace <text>
+
+See image below for demonstration. 
+![pride demonstration](https://cloud.githubusercontent.com/assets/945693/6345257/cdde70ec-bbb8-11e4-8ead-3b57bcf827ef.png)
+Note: Colors may appear differently in different terminals.
+I have done my best to match colors, but a lot of the flag colors simply aren't
+currently supported in irssi. Some look better/closer than others.
 
 ## Installation
 To use, place selected scripts in ~/.irssi/scripts. In irssi, type
@@ -55,4 +86,12 @@ To use, place selected scripts in ~/.irssi/scripts. In irssi, type
 ```
 Note: all scripts are standalone. You need only load the script with the 
 commands you want.
-Once the scripts are loaded, you can use the commands.
+Once the scripts are loaded, use as directed in usage notes.
+
+## Configuration
+Spectrum and pride both allow you to customize how wide the color stripes are. To
+change, simply change number in the line
+```
+my $colorspan = 4; #how many characters makes up each color before switching
+```
+to your desired width.
---
+++ pride.pl
@@ -0,0 +1,192 @@
+#!/usr/bin/perl -w
+
+# USAGE:
+# /pride <text>
+# output colored text in the pride colorscheme
+# unlike the spectrum script, these always start on the same color
+# if the first word of the text is /topic, /kick, or /me, completes those 
+# commands with colored text.
+#
+# The following commands work in the same way with their respective colors:
+# /trans <text>
+# /genderqueer <text>
+# /bi <text>
+# /pan <text>
+# /poly <text>
+# /ace <text>
+# /lightace <text>
+#
+# I have done my best to match colors, but a lot of the flag colors simply aren't
+# currently supported in irssi. Some look better/closer than others.
+
+# Modified from rainbow.pl by Jakub Jankowski <shasta@atn.pl>
+# for Irssi 0.7.98.4 and newer
+
+use strict;
+use vars qw($VERSION %IRSSI);
+
+$VERSION = "1.0";
+%IRSSI = (
+    authors     => 'Laurel "Kona" Hart',
+    contact     => 'laurel.elise.hart@gmail.com',
+    name        => 'pride',
+    description => 'Print colored text in gsrm/lgbtq/quiltbag colorschemes.',
+    license     => 'GNU GPLv2 or later',
+#    url         => '',
+);
+
+use Irssi;
+use Irssi::Irc;
+
+#Config:
+my $colorspan = 4; #how many characters makes up each color before switching
+
+# colors list (foregrounds)
+#  0 == white
+#  5 == red
+#  4 == light red
+#  7 == orange
+#  8 == yellow
+#  9 == light green
+#  3 == green
+# 10 == cyan
+# 11 == light cyan
+# 12 == light blue
+#  2 == blue
+#  6 == magenta
+# 13 == light magenta
+#  1 == black
+# 14 == gray
+# 15 == light gray
+
+my @pride = ('5', '4', '7', '8', '9', '3', '10', '11', '12', '2', '6', '13');
+my @trans = ('12', '4', '0', '4', '12');
+my @genderqueer = ('13', '0', '3');
+my @bi = ('4', '4', '6', '2', '2');
+my @pan = ('4', '8', '11');
+my @poly = ('4', '3', '12');
+my @ace = ('1', '14', '0', '6');
+my @lightace = ('14', '15', '0', '6');
+
+# str make_colors($colorscheme, $string)
+# returns a string colored according to colorscheme, starting at a random point
+# in the colorscheme
+sub make_colors {
+	my ($colorscheme, $string) = @_;
+	Encode::_utf8_on($string);
+	my $newstr = "";
+	my $color = -1;
+
+	my $i = 0;
+	for (my $c = 0; $c < length($string); $c++) {
+		my $char = substr($string, $c, 1);
+		#don't color spaces
+		if ($char eq ' ') {
+			$newstr .= $char;
+			next;
+		}
+		if ($i % $colorspan == 0){  
+			$color++;	
+			$color = $color % @{$colorscheme};
+			$newstr .= "\003";
+			$newstr .= sprintf("%02d", $colorscheme->[$color]);
+		}
+		$newstr .= $char;
+		$i++;
+	}
+
+	return $newstr;
+}
+
+# void colorify($text, $server, $destination, $colorscheme)
+# handles /msg, /me, /topic, and /kick and colors string
+sub colorify {
+	my ($text, $server, $dest, $colorscheme) = @_;
+
+	if (!$server || !$server->{connected}) {
+		Irssi::print("Not connected to server");
+		return;
+	}
+
+	if ($dest) {
+		my ($command, $message) = split(/ +/, $text, 2);
+		$command = lc $command;
+		if ($dest->{type} eq "CHANNEL") { 
+			if ($command eq "/me") {
+				$dest->command("/me " . 
+				make_colors($colorscheme, $message));
+			} elsif ($command eq "/topic") {
+				$dest->command("/topic " . 
+				make_colors($colorscheme, $message));
+			} elsif ($command eq "/kick") {
+				my ($nick, $reason) = split(/ +/, $message, 2);
+				return unless $nick;
+				$reason = "Irssi power!" if (
+					$reason =~ /^[\ ]*$/);
+				$dest->command("/kick " . $nick . " " . 
+				make_colors($colorscheme, $reason));
+			} else {
+				$dest->command("/msg " . $dest->{name} . " " . 
+				make_colors($colorscheme, $text));
+			}
+			
+		} elsif ($dest->{type} eq "QUERY") {
+			if ($command eq "/me") {
+				$dest->command("/me " . make_colors($message));
+			} else {
+				$dest->command("/msg " . $dest->{name} . " " . 
+				make_colors($colorscheme, $text));
+			}
+		}
+
+	} 
+}
+
+# void pride($text, $server, $destination)
+sub pride {
+	colorify(@_, \@pride);
+}
+
+# void trans($text, $server, $destination)
+sub trans {
+	colorify(@_, \@trans);
+}
+
+# void genderqueer($text, $server, $destination)
+sub genderqueer {
+	colorify(@_, \@genderqueer);
+}
+
+# void bi($text, $server, $destination)
+sub bi {
+	colorify(@_, \@bi);
+}
+
+# void pan($text, $server, $destination)
+sub pan {
+	colorify(@_, \@pan);
+}
+
+# void poly($text, $server, $destination)
+sub poly {
+	colorify(@_, \@poly);
+}
+
+# void ace($text, $server, $destination)
+sub ace {
+	colorify(@_, \@ace);
+}
+
+# void ace($text, $server, $destination)
+sub lightace {
+	colorify(@_, \@lightace);
+}
+
+Irssi::command_bind("pride", "pride");
+Irssi::command_bind("trans", "trans");
+Irssi::command_bind("genderqueer", "genderqueer");
+Irssi::command_bind("bi", "bi");
+Irssi::command_bind("pan", "pan");
+Irssi::command_bind("poly", "poly");
+Irssi::command_bind("ace", "ace");
+Irssi::command_bind("lightace", "lightace");
